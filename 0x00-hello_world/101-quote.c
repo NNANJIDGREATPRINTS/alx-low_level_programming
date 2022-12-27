d3a0eb0b74510174cb5a962f91c5af57f87c2daa
hanid <dgreatprints.com>
2022-12-27 15:01:01 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,6 @@
 
 /**
  * main - print the string in the put function
- *
  * Description: using the main function
  * this program prints "and that piece of art is useful - Dora Korpar
  * Return: 0
