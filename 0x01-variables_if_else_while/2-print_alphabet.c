3b38606e7c9faaaeeefbfe96ab833f7a0ef2c994
hanid <dgreatprints.com>
2023-01-01 22:50:19 -0800

    task3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or negative
+ *
+ *  Description: using the main function
+ *  this is a program that prints "Programming is a positive, zero, or negative
+ *  return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+
+	{
+
+			putchar(ch);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}	
