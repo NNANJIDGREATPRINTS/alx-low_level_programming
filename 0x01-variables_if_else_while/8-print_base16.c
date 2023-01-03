57ffd54206666b89f2382cc61acec2fce43039ba
hanid <dgreatprints.com>
2023-01-03 01:40:15 -0800

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,17 +4,21 @@
  *
  * Descrption: using the main function
  * this program prints "Programming is positive, zero, or negative
- * Return : 0
+ * Return: 0
  */
 
 int main(void)
 {
 	char c;
 
-	for (c = 'z' ; c >= 'a'; c--)
+	for (c = '0' ; c <= '9' ; c++)
 	{
 		putchar(c);
 	}
-		putchar('\n');
-		return (0);
+		for (c = 'a' ; c <= 'f' ; c++)
+		{
+			putchar(c);
+		}
+	putchar('\n');
+	return (0);
 }
