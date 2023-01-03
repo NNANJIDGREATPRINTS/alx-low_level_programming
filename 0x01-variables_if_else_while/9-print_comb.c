58e01f012a58c68c69aa89495dd27534f39c4c82
hanid <dgreatprints.com>
2023-01-03 13:40:43 -0800

    task12

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print single digit number
+ *
+ *  Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 10; i < 20; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i != 19)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
