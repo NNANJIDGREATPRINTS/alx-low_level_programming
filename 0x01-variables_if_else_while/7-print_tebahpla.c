d7e6c0bf46357e1fe36193a0f83fb2014a03912f
hanid <dgreatprints.com>
2023-01-03 00:59:04 -0800

    mandatory task

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 /**
  * main - main block
  * Description: prints all single digit numbers of base 10
  * starting from 0, followed by a new line.
- * Return: 0
+ * Return : 0
  */
+
 int main(void)
 {
-	int c = 0;
+	char c = 'z';
 
-	while (c < 10)
+	while (c >= 'a')
 	{
-		putchar(48 + c)
-		c++;
+		putchar(c);
+		c--;
 	}
 
-	putchar('\n');
-	return (0);
+		putchar('\n');
+		return (0);
 }
